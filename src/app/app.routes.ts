2b31c503128dc456088225d9f3b2a85661a3aba7
Haris <harisasif1592@gmail.com>
2025-07-11 12:33:00 +0500

    Designed Layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { Component } from '@angular/core';
+import { DashboardComponent } from './features/dashboard/dashboard.component';
+import { ProductsComponent } from './features/products/products.component';
+import { SuppliersComponent } from './features/suppliers/suppliers.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'', component:LayoutComponent, children: [
+            {
+                path:'dashboard', component:DashboardComponent
+            },
+            {
+                path:'products', component:ProductsComponent
+            },
+            {
+                path:'suppliers', component:SuppliersComponent
+            },
+            {
+                path:'', redirectTo:'dashboard', pathMatch:'full'
+            }
+        ]
+    }
+];
---
+++ src/app/layout/layout.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  imports: [RouterLink, RouterLinkActive, RouterOutlet],
+  templateUrl: './layout.component.html',
+  styleUrl: './layout.component.css'
+})
+export class LayoutComponent {
+
+}
