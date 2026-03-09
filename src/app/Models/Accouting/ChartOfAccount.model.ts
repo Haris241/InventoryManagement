c0d3d63c140550ded4f5ae904ea998ae5791d75c
Khurram ExSoft <khurram.abbasi@ezsoftpk.com>
2026-03-09 09:51:52 +0500

    COA Model

--- src/app/Models/Accouting/ChartOfAccount.model.ts
+++ src/app/Models/Accouting/ChartOfAccount.model.ts
@@ -1,32 +1,24 @@
-//  public class CreateCOA
-//  {
-//      [Required(ErrorMessage = "Name is required")]
-//      public string? Name { get; set; }= string.Empty;
-//      public int? ParentId { get; set; }
-//      [Required(ErrorMessage = "Account kind is required")]
-//      public AccountKind? Kind { get; set; }
+export interface CreateCOA {
+    name: string,
+    parentId: number,
+    kind: AccountKind,
+    category: AccountType,
+    openingBalance: number
+}
+export enum AccountType {
+    Asset = 1,
+    Liability = 2,
+    Equity = 3,
+    Revenue = 4,
+    Expense = 5
+}
 
-//      [Required(ErrorMessage = "Account category is required")]
-//      public AccountType? Category { get; set; }
-//      public decimal? OpeningBalance { get; set; }
-//  }
-//  public enum AccountType
-//     {
-//         Asset = 1,
-//         Liability = 2,
-//         Equity = 3,
-//         Revenue = 4,
-//         Expense = 5
-//     }
+export enum BalanceType {
+    Debit = 1,
+    Credit = 2
+}
 
-//     public enum BalanceType
-//     {
-//         Debit = 1,  // Increases with Debit (Assets, Expenses)
-//         Credit = 2  // Increases with Credit (Liabilities, Equity, Revenue)
-//     }
-
-//     public enum AccountKind
-//     {
-//         Group = 1,  // A folder for other accounts (Summary only)
-//         Ledger = 2  // A postable account for transactions (The "Leaf")
-//     }
+export enum AccountKind {
+    Group = 1,
+    Ledger = 2
+}
