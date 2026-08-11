ee3a6478ec0dc5136dc1ac4ddf99a30d3fd8e89a
alihon719na-rgb <alihon719na@gmail.com>
2026-08-11 17:49:49 +0500

    Added AccountDashboard Model

---
+++ src/app/Models/Accouting/AccountDashboard.model.ts
@@ -0,0 +1,38 @@
+import { BalanceData } from "./AccountReports.model";
+
+export interface AccountDashBoardData {
+    totalCash: BalanceData;
+    totalBank: BalanceData;
+    totalRevenue: BalanceData;
+    totalExpenses: BalanceData;
+    netProfit: BalanceData;
+    topAccountsPayable: AccountBalanceData[];
+    topAccountsReceivable: AccountBalanceData[];
+    revenueExpenseTrend: RevenueExpenseTrendPoint[];
+    expenseBreakdown: CategoryBreakdownItem[];
+}
+export interface AccountBalanceData {
+    name: string;
+    balance: BalanceData;
+}
+export interface RevenueExpenseTrendPoint {
+    period: Date;
+    revenue: number;
+    expense: number;
+}
+export interface CategoryBreakdownItem {
+    accountId: number;
+    categoryName: string;
+    amount: number;
+}
+
+export interface AccountsDashBoardSearch {
+    filterType?: AccountDashboardFilterType;
+    refresh: boolean;
+}
+export enum AccountDashboardFilterType {
+    Today = 1,
+    ThisWeek = 2,
+    ThisMonth = 3,
+    SixMonth = 4
+}
